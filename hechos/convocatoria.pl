25506101d18f851d3b69e2c306d624159a70ac73
AgustinDuelli <agustinsduelli@gmail.com>
2024-06-21 19:12:34 -0300

    Add Convocatorias para proyectos

---
+++ hechos/convocatoria.pl
@@ -0,0 +1,4 @@
+% convocatoria(id, dia, mes, año, tipo_convocatoria)
+convocatoria(1, 1, 1, 2020, desarrollo).
+convocatoria(2, 1, 1, 2020, iniciacion_investigacion).
+convocatoria(3, 12, 7, 2020, desarrollo).
